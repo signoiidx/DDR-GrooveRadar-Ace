9adae938d0d8949d5fc7c3a1323d1d1e75caf7ee
signoiidx <signo.iidx@gmail.com>
2020-10-13 09:40:25 +0900

    webscrape useful data and generate data import script for MATLAB

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# ddr_grooveradar_ace
-a tool for evaluating DDR skill in DDR A.
+# DDR Groove Radar Ace
+  DDR Groove Radar Ace is a tool for evaluating DDR skill in DDR A.
+
+## Installation
+- download release
+- unzip
+
+## Usage
+- prepare file
+- launch MATLAB
+- run main.m
+
+## Contributing
+  If you find something, please open an issue.
+
+## Disclaimer
+- This tool is not affiliated with Konami Amusement Co., Ltd.
+
+## Acknowledgement
+Following data has been used to create the project.
+- [BEMANIwiki 2nd](https://bemaniwiki.com/index.php?DanceDanceRevolution%20A20)
+  - Groove radar value
+  - Notes count
+- [SkillAttack](http://skillattack.com/sa4/)
+  - Current available chart list
+  - Music ID management system
+
+## License
+  This product is __purified__ by [the MIT License](https://opensource.org/licenses/mit-license.php).
---
+++ ddr_import_diffinfo.m
@@ -0,0 +1,32 @@
+%% テキスト ファイルからのデータのインポート
+% 次のテキスト ファイルからデータをインポートするスクリプト:
+%
+%    ファイル名: ddr_diffinfo_list.csv
+%
+% MATLAB からの自動生成日: 2020/10/13 06:05:08
+
+%% インポート オプションの設定およびデータのインポート
+opts = delimitedTextImportOptions("NumVariables", 13);
+
+% 範囲と区切り記号の指定
+opts.DataLines = [1, Inf];
+opts.Delimiter = ",";
+
+% 列名と型の指定
+opts.VariableNames = ["id", "Il6980di8P89lil1PDIqqIbiq1QO8lQ", "besp", "bsp", "dsp", "esp", "csp", "bdp", "ddp", "edp", "cdp", "title", "artist"];
+opts.VariableTypes = ["double", "string", "double", "double", "double", "double", "double", "double", "double", "double", "double", "string", "string"];
+
+% ファイル レベルのプロパティを指定
+opts.ExtraColumnsRule = "ignore";
+opts.EmptyLineRule = "read";
+
+% 変数プロパティを指定
+opts = setvaropts(opts, ["Il6980di8P89lil1PDIqqIbiq1QO8lQ", "title", "artist"], "WhitespaceRule", "preserve");
+opts = setvaropts(opts, ["Il6980di8P89lil1PDIqqIbiq1QO8lQ", "title", "artist"], "EmptyFieldRule", "auto");
+
+% データのインポート
+ddr_all_songs = readtable("./data/ddr_diffinfo_all.csv", opts);
+
+
+%% 一時変数のクリア
+clear opts
